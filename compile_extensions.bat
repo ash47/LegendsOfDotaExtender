8242db9a6f8fd4d824001430afeefaa1e559ddc8
ash47 <ash.fourtyseven@gmail.com>
2016-07-08 22:06:57 +1000

    Started to write the extender

---
+++ compile_extensions.bat
@@ -0,0 +1,20 @@
+
+
+:: Change back to the LoD root folder
+cd ..\..
+
+:: Cleanup old stuff
+rmdir /S /Q "dota\extended"
+
+:: Create a folder to house the custom distributions
+mkdir "dota\extended"
+mkdir "dota\extended\content"
+mkdir "dota\extended\game"
+mkdir "dota\extended\staging"
+
+:: Static stuff
+
+
+
+:: All done! Pause so output can be read
+pause
